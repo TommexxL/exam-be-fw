13afc69af7c7a0f0aadf57a186ebabfd4c82223c
TommexxL <tommexx_007@hotmail.com>
2026-03-18 14:58:46 +0100

    updated table and added the toggle button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,14 +10,40 @@
             <p>{{ session('success') }}</p>
     @endif
 
+<table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse; width: 100%;">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+
     @foreach ($courses as $course)
-        <div>
+    <!-- who needs styling when ye got... TABLES! -->
+    
             <tr>
                 <td>{{ $course->title }}</td>
                 <td>{{ $course->description }}</td>
                 <td>Status: {{ $course->active ? 'Active' : 'Inactive' }}</td>
+                <td>
+                    <form method="POST" action="{{ route('courses.toggle', $course->id) }}">
+                        @csrf
+                        @method('PATCH')
+
+                        <button type="submit">
+                            Toggle
+                        </button>
+                    </form>
+                </td>
             </tr>
-        </div>
+       
     @endforeach
+    
+    </tbody>
+</table>
 
 @endsection
