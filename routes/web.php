0a81a8e4ebec63695619b24436e9657eb00b5234
TommexxL <tommexx_007@hotmail.com>
2026-03-18 14:05:58 +0100

    completed routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/courseadd', function () {
+    return view('addCourse');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/courses', [CourseController::class, 'store']);
+
