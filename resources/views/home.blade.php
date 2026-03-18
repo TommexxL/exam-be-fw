4b8a6db197f7ec37b4de73143674d0f09058f608
TommexxL <tommexx_007@hotmail.com>
2026-03-18 14:19:39 +0100

    show index on home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<h2>Courses</h2>
+
+@if (session('success'))
+        <p>{{ session('success') }}</p>
+@endif
+
+@foreach ($courses as $course)
+    <div>
+        <tr>
+            <td>{{ $course->title }}</td>
+            <td>{{ $course->description }}</td>
+            <td>Status: {{ $course->active ? 'Active' : 'Inactive' }}</td>
+        </tr>
+    </div>
+@endforeach
